02022ab93cb0e6c646e57c16d354d7463b5f45aa
Kevin G. Tertuliano <robotnyte@gmail.com>
2021-08-01 11:55:51 -0300

    fix calculation of possibilities

--- src/generator.py
+++ src/generator.py
@@ -1,5 +1,4 @@
 from random import randint, choice
-from operator import mul
 from src.interface.menu import color_text
 from time import sleep
 
@@ -10,12 +9,12 @@ def factorial(num: int):
             tot *= n
         return tot
 
-def permutation(n: int, p: int):
-    all_characters = 71
-    return int(factorial(n)/ factorial(all_characters**p))
+def combination(p: int):
+    all_characteres = 71
+    return int(factorial(all_characteres)/ factorial(p) * factorial(all_characteres - p))
 
 
-def rand(word=None, symbols=False, only_numbers=False, numbers=False, uppers=False, position=False, limit=8):
+def generate(word=None, symbols=False, only_numbers=False, numbers=False, uppers=False, position=False, limit=8):
     """
 Generates a random password of 8 or more characters between letters and numbers.
     name = word you want to add to the generator:
@@ -27,100 +26,84 @@ Generates a random password of 8 or more characters between letters and numbers.
     pos = Changes the position of the name parameter.
 """
 
-    result: str = ''
     letters = (
         'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
         'u', 'v', 'w', 'x', 'y', 'z'
     )
-    # geração de números
-    if only_numbers:
-        for c in range(0, limit):
-            result += str(randint(0, 9))
-    else:
-        # geração da palavra-chave
-        if word is not None:
-            lenght = len(word)
-            if numbers:
-                for c in range(0, limit - lenght + 1):
-                    result += str(randint(0, 9))
-            elif not numbers:
-                for c in range(0, limit - lenght + 1):
-                    result += choice(letters)
-            if position:
-                result = word + result
-            elif not position:
-                result += word
+    while True:
+        result: str = ''
+        # geração de números
+        if only_numbers:
+            for c in range(0, limit):
+                result += str(randint(0, 9))
+        else:
+            # geração da palavra-chave
+            if word is not None:
+                lenght = len(word)
+                if numbers:
+                    for _ in range(0, limit - lenght + 1):
+                        result += str(randint(0, 9))
+                elif not numbers:
+                    for _ in range(0, limit - lenght + 1):
+                        result += choice(letters)
+                if position:
+                    result = word + result
+                elif not position:
+                    result += word
 
-        # parte da geração aleatória
-        if word is None:
-            letters2 = list(letters[:])
-            if uppers:
-                for letter in letters:
-                    letters2.append(letter.upper())
-            if symbols:
-                letters2.append('#')
-                letters2.append('@')
-                letters2.append('+')
-                letters2.append('%')
-                letters2.append('$')
-                letters2.append('!')
-                letters2.append('?')
-                letters2.append('&')
-                letters2.append('*')
-            if numbers:
-                for c in range(9):
-                    letters2.append(c)
-            for c in range(limit):
-                result += str(choice(letters2))
-    return result
+            # parte da geração aleatória
+            if word is None:
+                letters2 = list(letters[:])
+                if uppers:
+                    for letter in letters:
+                        letters2.append(letter.upper())
+                if symbols:
+                    letters2.append('#')
+                    letters2.append('@')
+                    letters2.append('+')
+                    letters2.append('%')
+                    letters2.append('$')
+                    letters2.append('!')
+                    letters2.append('?')
+                    letters2.append('&')
+                    letters2.append('*')
+                if numbers:
+                    for c in range(9):
+                        letters2.append(c)
+                for c in range(limit):
+                    result += str(choice(letters2))
+        yield result
 
 
 def main(archive: str, **kwargs):
-    word = kwargs.get("word")
-
-    simb = kwargs.get("symbols")
-    if simb is None:
-        simb = False
-
-    only_num = kwargs.get("only_num")
-    if only_num is None:
-        only_num = False
-
-    num = kwargs.get("numbers")
-    if num is None:
-        num = False
-
-    cap = kwargs.get("uppers")
-    if cap is None:
-        cap = False
-
-    pos = kwargs.get("position")
-    if pos is None:
-        pos = False
-
+    print(kwargs)
     limit = kwargs.get("limit")
+    word = kwargs.get("word")
     if limit is None:
         limit = 8
-
-    res_combine = factorial(limit)
     if word is not None:
-        rest = ''
-        lenght = len(word)
-        for _ in range(0, limit - lenght + 1):
-            rest += str(randint(0, 9))
-        res_combine = factorial(len(rest))
+        limit -= len(word)
+    repeat = limit
 
     possibilities = {}
-    print(f"Were calculated {res_combine} possibilities")
+    n_possibilites = combination(repeat)
+    print(f"Were calculated {color_text('yellow', n_possibilites)} possibilities.")
     print(color_text('white', 'generating ...'))
-    sleep(3)
-    with open(archive, "w+") as out:
-        for c in range(res_combine):
-            retorn = rand(word, simb, only_num, num, cap, pos, limit)
-            possibilities[retorn] = 0
-            if possibilities[retorn] > 1:
-                print(color_text("red", retorn))
-            else:
-                print(color_text('green', retorn))
-                possibilities[retorn] += 1
-                out.write(retorn)
+    try:
+        print(color_text(color_text("yellow", "Press Ctrl + C to stop the script at any time.")))
+        sleep(3)
+    except KeyboardInterrupt:
+        print("Script stopped by user.")
+    else:
+        with open(archive, "w+") as file:
+            while n_possibilites > 0:
+                for out in generate(kwargs):
+                    possibilities[out] = 0
+                    if possibilities[out] > 1:
+                        print(color_text("red", out))
+                        pass
+                    else:
+                        possibilities[out] += 1
+                        n_possibilites -= 1
+                        file.write(out)
+                        print(color_text("white", out))
